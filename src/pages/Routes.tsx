28f41311a218ebcd9ae0b1882e17818b2442da98
Demis Russo <demisjunior@hotmail.com>
2024-07-30 14:10:08 -0300

    add sign-up page

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import { Dashboard } from './app/dashboard'
 import { SignIn } from './auth/sign-in'
+import { SignUp } from './auth/sign-up'
 import { AppLayout } from './layouts/app'
 import { AuthLayout } from './layouts/auth'
 
@@ -15,7 +16,10 @@ export function Routes() {
     {
       path: '/',
       element: <AuthLayout />,
-      children: [{ path: '/sign-in', element: <SignIn /> }],
+      children: [
+        { path: '/sign-in', element: <SignIn /> },
+        { path: '/sign-up', element: <SignUp /> },
+      ],
     },
   ])
 
